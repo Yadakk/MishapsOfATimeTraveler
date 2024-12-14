be915b8b70b3ee470924ec142ed9a97f1f6beb3f
cannedenuum <cannedenuum@hotmail.com>
2024-12-14 15:50:54 +0300

    feat: Game can be paused and quitted

---
+++ Assets/Source/MOATT/Levels/UI/GamePauser.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MOATT.Levels.UI
+{
+    public class GamePauser : MonoBehaviour
+    {
+        [SerializeField] private GameObject menu;
+
+        private void Start()
+        {
+            menu.SetActive(false);
+        }
+
+        public void Toggle()
+        {
+            bool isPaused = Time.timeScale == 0f;
+            Time.timeScale = isPaused ? 1f : 0f;
+            menu.SetActive(!isPaused);
+        }
+
+        private void OnDestroy()
+        {
+            Time.timeScale = 1f;
+        }
+    }
+}
---
+++ Assets/Source/MOATT/UI/SceneChanger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace MOATT.UI
+{
+    public class SceneChanger : MonoBehaviour
+    {
+        [SerializeField] private GameObject loadingScreenPrefab;
+        [SerializeField] private Transform canvasTransform;
+        [SerializeField] private string sceneName;
+
+        private Slider loadingSlider;
+        private AsyncOperation loadingOperation;
+
+        private void Update()
+        {
+            if (loadingSlider == null) return;
+            if (loadingOperation == null) return;
+            loadingSlider.value = loadingOperation.progress;
+        }
+
+        public void ChangeScene()
+        {
+            GameObject loadingScreen = Instantiate(loadingScreenPrefab, canvasTransform);
+            loadingScreen.transform.SetAsLastSibling();
+            loadingSlider = loadingScreen.GetComponentInChildren<Slider>();
+            loadingOperation = SceneManager.LoadSceneAsync(sceneName);
+        }
+    }
+}

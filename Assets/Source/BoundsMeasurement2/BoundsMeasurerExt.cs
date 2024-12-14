ba5c923d7b2a1eab0264ec7607fc9bf5a3199ef7
cannedenuum <cannedenuum@hotmail.com>
2024-12-14 21:56:01 +0300

    fix: Moved former submodules to another folder

---
+++ Assets/Source/BoundsMeasurement2/BoundsMeasurerExt.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BoundsMeasurement
+{
+    public static class BoundsMeasurerExt
+    {
+        public static Bounds GetBounds(this Renderer[] renderers)
+        {
+            Bounds result = new();
+            bool initFlag = false;
+
+            foreach (var item in renderers)
+            {
+                if (!initFlag)
+                {
+                    result = item.bounds;
+                    initFlag = true;
+                    continue;
+                }
+
+                result.Encapsulate(item.bounds);
+            }
+
+            return result;
+        }
+
+        public static Bounds GetBounds(this GameObject gameObject)
+        {
+            return GetBounds(gameObject.GetComponentsInChildren<Renderer>());
+        }
+
+        public static Bounds GetBounds(this Component component)
+        {
+            return GetBounds(component.gameObject);
+        }
+    }  
+}

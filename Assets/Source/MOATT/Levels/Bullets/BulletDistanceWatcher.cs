4f1e632c9b77bc49fd78b9e2be8ee2ed22d6c9ee
cannedenuum <cannedenuum@hotmail.com>
2024-12-16 15:34:37 +0300

    fix: Bullets cache origin position now

--- Assets/Source/MOATT/Levels/Bullets/BulletDistanceWatcher.cs
+++ Assets/Source/MOATT/Levels/Bullets/BulletDistanceWatcher.cs
@@ -9,11 +9,13 @@ namespace MOATT.Levels.Bullets
     using Buildings.Turrets;
     using Units.Range;
 
-    public class BulletDistanceWatcher : ITickable
+    public class BulletDistanceWatcher : IInitializable, ITickable
     {
         private readonly BuildingFacade origin;
         private readonly BulletFacade facade;
         private readonly UnitRange unitRange;
+
+        private Vector3 originPosition;
 
         public BulletDistanceWatcher(BuildingFacade origin, BulletFacade facade, UnitRange unitRange)
         {
@@ -22,9 +24,14 @@ namespace MOATT.Levels.Bullets
             this.unitRange = unitRange;
         }
 
+        public void Initialize()
+        {
+            originPosition = origin.transform.position;
+        }
+
         public void Tick()
         {
-            float distance = Vector3.Distance(origin.transform.position, facade.transform.position);
+            float distance = Vector3.Distance(originPosition, facade.transform.position);
             if (distance <= unitRange.Range) return;
             facade.Destroy();
         }

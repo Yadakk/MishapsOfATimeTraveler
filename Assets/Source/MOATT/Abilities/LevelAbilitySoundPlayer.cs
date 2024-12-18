c0f8893617e1a401626536d5c5cc47c410b6d982
cannedenuum <cannedenuum@hotmail.com>
2024-12-18 15:06:18 +0300

    feat: Added ability sounds

---
+++ Assets/Source/MOATT/Abilities/LevelAbilitySoundPlayer.cs
@@ -0,0 +1,40 @@
+﻿using MOATT.Sound;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace MOATT.Abilities
+{
+    public class LevelAbilitySoundPlayer
+    {
+        private readonly SceneSoundPlayer sceneSoundPlayer;
+        private readonly Settings settings;
+
+        public LevelAbilitySoundPlayer(SceneSoundPlayer sceneSoundPlayer, Settings settings)
+        {
+            this.sceneSoundPlayer = sceneSoundPlayer;
+            this.settings = settings;
+        }
+
+        public void PlayCharged()
+        {
+            sceneSoundPlayer.PlayOneShot(settings.chargedSound, settings.chargedSoundVolume);
+        }
+
+        public void PlayActivated()
+        {
+            sceneSoundPlayer.PlayOneShot(settings.activatedSound, settings.activatedSoundVolume);
+        }
+
+        [Serializable]
+        public class Settings
+        {
+            public AudioClip chargedSound;
+            [Range(0f, 1f)] public float chargedSoundVolume = 0.5f;
+            public AudioClip activatedSound;
+            [Range(0f, 1f)] public float activatedSoundVolume = 1f;
+        }
+    }
+}
---
+++ Assets/Source/MOATT/Sound/SceneSoundPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MOATT.Sound
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class SceneSoundPlayer : MonoBehaviour
+    {
+        private AudioSource audioSource;
+
+        private void Awake()
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        public void PlayOneShot(AudioClip clip, float volume = 1f)
+        {
+            audioSource.PlayOneShot(clip, volume);
+        }
+    }
+}

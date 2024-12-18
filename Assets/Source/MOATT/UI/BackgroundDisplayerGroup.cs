31c56b23325c9240dd58b5482872757a515980f4
cannedenuum <cannedenuum@hotmail.com>
2024-12-18 13:09:21 +0300

    feat: Background can be changed on level button hover

---
+++ Assets/Source/MOATT/UI/BackgroundDisplayerGroup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MOATT.UI
+{
+    public class BackgroundDisplayerGroup : MonoBehaviour
+    {
+        [SerializeField] private Image image;
+
+        private Sprite defaultBg;
+
+        private void Awake()
+        {
+            defaultBg = image.sprite;
+        }
+
+        public void SetBackground(Sprite background)
+        {
+            image.sprite = background != null ? background : defaultBg;
+        }
+    }
+}

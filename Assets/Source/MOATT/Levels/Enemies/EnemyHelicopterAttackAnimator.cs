74187d1695334d827a8bd7c3aaabe83c0f130276
cannedenuum <cannedenuum@hotmail.com>
2024-12-20 02:56:44 +0300

    feat: Added helicopter laser attack animation

---
+++ Assets/Source/MOATT/Levels/Enemies/EnemyHelicopterAttackAnimator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+using TimeTimers;
+using static UnityEngine.GraphicsBuffer;
+
+namespace MOATT.Levels.Enemies
+{
+    public class EnemyHelicopterAttackAnimator : MonoBehaviour, IAttackAnimator
+    {
+        [SerializeField] private LineRenderer lineRenderer;
+
+        private Timer timer;
+        private bool timerFlag;
+
+        [Inject]
+        public void Construct(Timer timer)
+        {
+            this.timer = timer;
+        }
+
+        private void Update()
+        {
+            if (!timerFlag) return;
+            lineRenderer.enabled = timer.Elapsed <= 0.1f;
+            lineRenderer.SetPosition(0, transform.position);
+        }
+
+        public void Play(Vector3 target)
+        {
+            timer.Reset();
+            timerFlag = true;
+
+            lineRenderer.SetPosition(1, target);
+        }
+    }
+}
